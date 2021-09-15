1ec873b0820ca5588c96cd7562ad098677970265
AbdullahSarder <57371591+AbdullahSarder@users.noreply.github.com>
2021-09-15 14:48:34 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -94,6 +94,10 @@ const setInnerText = (id, value) => {
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
+    if (priceConverted < 200) {
+    setInnerText("delivery-charge", 20);
+    setInnerText("total-tax", priceConverted * 0.2);
+  }
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
     setInnerText("total-tax", priceConverted * 0.2);
